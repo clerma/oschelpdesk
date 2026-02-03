089a7811306edf991919be53785218d6f72cab80
Carlos <carlos@accoladesit.com>
2026-02-02 21:05:11 -0600

    update to page loader

--- js/pre-loader.js
+++ js/pre-loader.js
@@ -4,10 +4,9 @@
   /*============= preloader js css =============*/
   var cites = [];
   cites[0] =
-    "We design Docy for the readers, optimizing not for page views or engagement";
-  cites[1] = "Docy turns out that context is a key part of learning.";
-  cites[2] = "You can create any type of product documentation with Docy";
-  cites[3] = "Advanced visual search system powered by Ajax";
+    "OSC Opelousas Help Desk";
+  cites[1] = "Help Desk turns out that context is a key part of learning.";
+  cites[2] = "Helping you find solutions to your problems";
   var cite = cites[Math.floor(Math.random() * cites.length)];
   $("#preloader p").text(cite);
   $("#preloader").addClass("loading");
